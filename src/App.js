0176a7b6e422e46c256fa835031acefa4e7602e4
Jesus Rojas <jesusnicotra@gmail.com>
2023-07-20 17:30:34 -0400

    correcciones generales

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@
 //import ProductView from './pages/productView/ProductView';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from "./pages/home/Home";
@@ -17,6 +17,9 @@ import Dashboard from "./pages/admin/Dashboard";
 import Footer from "./components/footer/Footer";
 import ShoppingCart from "./pages/shoppingCart/ShoppingCart";
 import Company from "./pages/Company/Company";
+import UserInfo from "./pages/user/UserInfo";
+import Contact from "./pages/contact/Contact";
+import CambiosDevoluciones from "./pages/cambiosYDevoluciones/CambiosDevoluciones";
 
 function App() {
    return (
@@ -27,11 +30,13 @@ function App() {
             <Route path="/carrito" element={<ShoppingCart />} />
             <Route path="/empresa" element={<Company />} />
             <Route path="/admin" element={<Dashboard />} />
+            <Route path="/usuario" element={<UserInfo />} />
+            <Route path="/contacto" element={<Contact />} />
+            <Route path="/cambiosdevoluciones" element={<CambiosDevoluciones />} />
          </Routes>
          <Footer />
       </BrowserRouter>
    );
-
 }
 
 export default App;
