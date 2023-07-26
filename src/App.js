6b126831098598669eb571c8c85ae24db47b75d7
gatowaton <pablocavada.t@gmail.com>
2023-07-26 15:19:31 -0400

    frontbackV2

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import Home from './pages/home/Home';
-import Nav from "./components/nav/Nav"
+import Home from "./pages/home/Home";
+import Nav from "./components/nav/Nav";
 import Dashboard from "./pages/admin/Dashboard";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import { AuthProvider } from "./Context/AuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Nav/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/admin" element={<Dashboard/>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="/admin" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
