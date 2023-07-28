0e97a99f14ad437d8ebfc0b80a4ced3fe7ec5fee
Josefa Laporte <laportejosefa@gmail.com>
2023-07-28 01:51:37 -0400

    correct

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-// import Nav from "./components/nav/Nav"
+//import Nav from "./components/nav/Nav"
 // import ProductCard from './components/productCard/ProductCard';
 // import ProductCard2 from './components/productCard/ProductCard2';
 // import Slider from "./components/slider/Slider"
@@ -9,15 +9,24 @@ import "bootstrap/dist/js/bootstrap.bundle";
 import Home from './pages/home/Home';
 import Nav from "./components/nav/Nav"
 import Dashboard from "./pages/admin/Dashboard";
+//import Registro from "./pages/registro/Registro";
+import InicioSesion from "./pages/iniciosesion/InicioSesion";
+//import DespachoRetiro from "./pages/despachoretiro/DespachoRetiro";
 
 function App() {
   return (
     <BrowserRouter>
     <Nav/>
-      <Routes>
+    {/* <Registro/>  */}
+    <InicioSesion/> 
+    {/* <DespachoRetiro/>  */}
+       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/admin" element={<Dashboard/>} />
-      </Routes>
+         <Route path="/admin" element={<Dashboard/>} />
+         {/* <Route path="/registro" element={<Registro/>} /> */}
+          
+
+      </Routes> *
     </BrowserRouter>
   );
 }
---
+++ src/pages/despachoretiro/DespachoRetiro.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./DespachoRetiro.css";
+ 
+function DespachoRetiro() {
+  return (
+
+    <div className="container">
+<div className="jumbotron">
+  <h1 className="display-4">Despacho y Retiro</h1>
+  <p className="lead">Lee atentamente los terminos de despacho y retiro de nuestra tienda. Ante cualquier consulta, no dudes en contactarnos.</p>
+  <hr className="my-4"></hr>
+  <p className="lead"></p>
+</div>
+
+      <div className="cont-d">
+        <h2>Servicio de Despacho</h2>
+        <p>Los despachos a domicilio solo se realizarán dentro de la comuna de Renca, en un plazo de 24 a 48 horas de confirmado el pedido. Una vez que se realice el despachp, se entregará un número de seguimiento.</p>
+      </div>
+
+      <div className="cont-r">
+        <h2>Servicio de Retiro</h2>
+        <p>El retiro en tienda está disponible las 24 horas hábiles posteriores a la confirmación del pedido. El retiro se debe realizar en nuestra tienda ubicada en Av. Balmaceda 4252, local A, Renca. El horario de retiro es de lunea a viernes de 10:00 a 17:00 hrs. </p>
+      </div>
+    </div>
+  );
+}
+
+export default DespachoRetiro;
