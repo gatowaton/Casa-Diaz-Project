f260d84a0e72d0386565880a99f1e3dbad70517a
Jesus Rojas <jesusnicotra@gmail.com>
2023-08-02 16:10:48 -0400

    Cards y ProductModel

--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -1,70 +1,72 @@
 const { Schema, model, default: mongoose } = require("mongoose");
 
-
-const productSchema = new Schema({
-    Titulo: {
-        type: String,
-        required: true
-    },
-    CodigoProducto:{
-        type: Number,
-        default: 0
-    },
-    CodigoAlternativo:{
-        type: Number,
-        default: 0
-    },
-    Categoria:{
-        type: String
-    },
-    SubCategoria:{
-        type: String
-    },
-    UnidadMedida:{
-        type: String
-    },
-    Marca: {
-        type: String,   
-    },
-    PrecioVentaNeto:{
-        type: Number,
-        required: true,
-        default: 0
-    },
-    PrecioVentaBruto:{
-        type: Number,
-        required: true,
-        default: 0
-    },
-    Exento:{
-        type: String 
-    },
-    CodigoImpuesto:{
-        type: Number,
-        default: 0 
-    },
-    Inventariable:{
-        type: String
-    },
-    Activo:{
-        type: String
-    },
-    Stock:{
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    Foto:{
-        type: String
-    },
-    date : {
-        type: Date,
-        default: Date.now
-    },
-},{
-    timestamps: true
-})
+const productSchema = new Schema(
+   {
+      Titulo: {
+         type: String,
+         required: true,
+      },
+      CodigoProducto: {
+         type: String,
+         default: "0",
+      },
+      CodigoAlternativo: {
+         type: String,
+         default: "0",
+      },
+      Categoria: {
+         type: String,
+      },
+      SubCategoria: {
+         type: String,
+      },
+      UnidadMedida: {
+         type: String,
+      },
+      Marca: {
+         type: String,
+      },
+      PrecioVentaNeto: {
+         type: Number,
+         required: true,
+         default: 0,
+      },
+      PrecioVentaBruto: {
+         type: Number,
+         required: true,
+         default: 0,
+      },
+      Exento: {
+         type: String,
+      },
+      CodigoImpuesto: {
+         type: Number,
+         default: 0,
+      },
+      Inventariable: {
+         type: String,
+      },
+      Activo: {
+         type: String,
+      },
+      Stock: {
+         type: Number,
+         required: true,
+         default: 0,
+      },
+      Foto: {
+         type: String,
+      },
+      date: {
+         type: Date,
+         default: Date.now,
+      },
+   },
+   {
+      timestamps: true,
+   }
+);
 
 const ProductModel = mongoose.model("Product", productSchema);
 
-module.exports = ProductModel
+module.exports = ProductModel;
