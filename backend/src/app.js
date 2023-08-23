86b241c7ea902917fbfce4b2ed1c50c337fe2c8b
gatowaton <pablocavada.t@gmail.com>
2023-08-23 16:41:34 -0400

    front

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors")
 const authRoutes = require("./routes/authRoute")
 const paymentRoutes = require("./routes/paymentRoute")
+const { FRONTEND_URL } = require("./config")
 
 
 
@@ -18,7 +19,7 @@ app.set("port", process.env.PORT || 4000);
 //middlewares: funciones que se ejecutan antes de que llegen a las rutas
 //cada vez que llegue una peticion a mi sv va a poder enviar y recibir datos
 app.use(cors({
-        origin: "https://casa-qwcy.onrender.com/",
+        origin: FRONTEND_URL,
         credentials: true
 }));
 //que tipo de datos enviara este sv: archivo en formato json// req.body a formato json
