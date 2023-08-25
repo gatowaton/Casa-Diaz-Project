e4ae55f9c4b293f69f05a366add3195073f183e8
fukkona <etlca1302@gmail.com>
2023-08-25 10:48:35 -0400

    paginación

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -2,40 +2,41 @@ import React from 'react';
 
 const Pagination = ({ productsPerPage, currentPage, setCurrentPage, totalProducts }) => {
   const totalPages = Math.ceil(totalProducts / productsPerPage);
+  const pageNumbers = [];
 
-  const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
 
   return (
-    <nav className="pagination is-rounded" role="navigation" aria-label="pagination">
+    <nav className="pagination is-rounded mb-6" role="navigation" aria-label="pagination">
       <a
-        className="pagination-previous"
+        className={`pagination-previous ${currentPage === 1 ? "is-disabled" : ""}`}
         onClick={() => setCurrentPage(currentPage - 1)}
-        disabled={currentPage === 1}
       >
-        Previous
+        Anterior
       </a>
       <a
-        className="pagination-next"
+        className={`pagination-next ${currentPage === totalPages ? "is-disabled" : ""}`}
         onClick={() => setCurrentPage(currentPage + 1)}
-        disabled={currentPage === totalPages}
       >
-        Next page
+        Siguiente página
       </a>
       <ul className="pagination-list">
-        {pageNumbers.map((pageNumber) => (
-          <li key={pageNumber}>
+        {pageNumbers.map(number => (
+          <li key={number}>
             <a
-              className={`pagination-link ${currentPage === pageNumber ? 'is-current' : ''}`}
-              aria-label={`Goto page ${pageNumber}`}
-              onClick={() => setCurrentPage(pageNumber)}
+              className={`pagination-link ${number === currentPage ? "is-current" : ""}`}
+              aria-label={`Ir a la página ${number}`}
+              onClick={() => setCurrentPage(number)}
             >
-              {pageNumber}
+              {number}
             </a>
           </li>
         ))}
       </ul>
     </nav>
   );
-};
+}
 
 export default Pagination;
--- src/pages/productList/productList.js
+++ src/pages/productList/productList.js
@@ -1,45 +1,49 @@
-import React, {useState} from "react";
+import React, { useState, useContext } from "react";
 import ProductCard from "../../components/productCard/ProductCard";
 import "./productList.css";
 import Filter from "../../components/filter/filter";
-import Pagination from "../../components/pagination/pagination"
-
-
+import Pagination from "../../components/pagination/pagination";
+import MyContext from "../../Context/MyContext";
 
 function ProductList() {
+  const { products } = useContext(MyContext);
 
-  const initialProducts = [{ProductCard}];
-
-  const [products, setProducts] = useState(initialProducts);
-  const [productsPerPage, setProductsPerPage] = useState(16);
+  const productsPerPage = 16;
   const [currentPage, setCurrentPage] = useState(1);
 
+  // Logic to calculate the index of the first and last products on the current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
   return (
-  <div>
-    
-    <div className="product-list-container">
-      <div className="product-filter">
-        Filtrar por
-        <br/>
-        <Filter/>
-      </div>
-      <div className="product-grid-container">
-        <div className="grid-row">
-          <ProductCard />        
+    <div>
+      <div className="product-list-container">
+        <div className="product-filter">
+          Filtrar por
+          <br />
+          <Filter />
         </div>
-       
+        <div className="product-grid-container">
+          <div className="grid-row">
+            {/* Map over the currentProducts array to render product cards */}
+            {currentProducts.map(product => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
       </div>
 
-     <div>
-      <Pagination
-        productsPerPage={productsPerPage}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        totalProducts={products.length} 
-      />
+      <div>
+        {/* Pass the necessary props to the Pagination component */}
+        <Pagination
+          productsPerPage={productsPerPage}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalProducts={products.length} // Use the length of the unfiltered products
+        />
       </div>
     </div>
-  </div>
   );
 }
 
